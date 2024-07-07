95a8f5a87025cb1b47e7027f4aa3f7de47a6e5d7
AlanLafuente12 <alan.alck12@gmail.com>
2024-07-07 09:12:12 -0400

    Comentarios en codigo no accesible

--- src/common/bcrypt.js
+++ src/common/bcrypt.js
@@ -4,7 +4,9 @@ import 'dotenv/config';
 
 export const encriptar = async(text) => {
     try {
+        // codigo que funciona
         return await bcrypt.hash(text, Number.parseInt(process.env.BCRYPT_SALTROUNDS));
+        // codigo que esta en npm pero no funciona
         await bcrypt.hash(text, +process.env.BCRYPT_SALTROUNDS).then((hash) => {
             return hash;
         });
@@ -16,7 +18,9 @@ export const encriptar = async(text) => {
 
 export const comparar = async(text, hash) => {
     try {
+        // codigo que funciona
         return await bcrypt.compare(text, hash);
+        // codigo que esta en npm pero no funciona
         await bcrypt.compare(text, hash).then((result) => {
             return result;
         });
