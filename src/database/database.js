caba3f67e2bfde49c6d876a83aa3c56eb1a4141c
AlanLafuente12 <alan.alck12@gmail.com>
2024-07-06 22:20:16 -0400

    fix: add ssl a sequelize

--- src/database/database.js
+++ src/database/database.js
@@ -6,6 +6,7 @@ export const sequelize = new Sequelize(
     process.env.DB_PASSWORD, {
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
-        logging: console.log
+        logging: console.log,
+        ssl: true
     }
 );
